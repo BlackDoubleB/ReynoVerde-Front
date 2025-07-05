ff6b2b9d4e5d858f725794b64f4b83e4a2e3dbf0
BlackDoubleB <blancablacido@gmail.com>
2025-07-05 00:32:13 -0500

    implementando modo oscuro/terminando responsive home

--- src/app/components/tema/tema.component.ts
+++ src/app/components/tema/tema.component.ts
@@ -1,17 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, effect, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-tema',
   imports: [CommonModule],
   templateUrl: './tema.component.html',
-  styleUrl: './tema.component.css'
+  styleUrl: './tema.component.css',
 })
 export class TemaComponent {
-  estado = signal(false);
+  estado = signal(false); // false = claro, true = oscuro
   claro = computed(() => !this.estado());
   oscuro = computed(() => this.estado());
+
   mover() {
-   this.estado.set(!this.estado());
+    this.estado.set(!this.estado());
+    document.documentElement.classList.toggle('dark', this.oscuro());
   }
 }
