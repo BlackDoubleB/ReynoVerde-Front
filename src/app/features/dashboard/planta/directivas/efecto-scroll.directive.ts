6c5362518404c59d3e571a6b9a97a4e7fac26b42
BlackDoubleB <blancablacido@gmail.com>
2025-07-26 00:02:47 -0500

    corrigiendo errores

--- src/app/features/dashboard/planta/directivas/efecto-scroll.directive.ts
+++ src/app/features/dashboard/planta/directivas/efecto-scroll.directive.ts
@@ -1,29 +1,3 @@
-// import { Directive, ElementRef, HostListener, OnDestroy } from '@angular/core';
-
-// @Directive({
-//   selector: '[appEfectoScroll]',
-// })
-// export class EfectoScrollDirective implements OnDestroy {
-//   private observer: IntersectionObserver;
-
-//   constructor(private el: ElementRef) {
-//     this.observer = new IntersectionObserver((entries) => {
-//       entries.forEach(entry => {
-//         entry.target.classList.toggle('mostrarScroll', entry.isIntersecting);
-//         entry.target.classList.toggle('ocultarScroll', !entry.isIntersecting);
-//       });
-//     },  {threshold:0.1}
-
-//   );
-
-//     this.observer.observe(this.el.nativeElement);
-//   }
-
-//   ngOnDestroy(): void {
-//     this.observer.disconnect();
-//   }
-
-// }
 import { Directive, ElementRef, HostListener, OnDestroy } from '@angular/core';
 
 @Directive({
@@ -33,18 +7,26 @@ export class EfectoScrollDirective implements OnDestroy {
   private observer: IntersectionObserver;
   private lastScrollTop = 0; // Para detectar dirección de scroll
   private scrollingDown = false;
+  private resizeObserver: ResizeObserver;
 
   constructor(private el: ElementRef) {
+    this.resizeObserver = new ResizeObserver((entries) => {
+      for (let entry of entries) {
+        const alto = entry.contentRect.height;
+        console.log('Nuevo alto:', alto);
+      }
+    });
+
+    this.resizeObserver.observe(this.el.nativeElement);
+
     this.observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
-         
           if (this.scrollingDown) {
             entry.target.classList.add('mostrarScroll');
-          } else{
+          } else {
             entry.target.classList.remove('mostrarScroll');
           }
-
         });
       },
       {
@@ -59,7 +41,8 @@ export class EfectoScrollDirective implements OnDestroy {
   onWindowScroll() {
     const currentScroll =
       window.pageYOffset || document.documentElement.scrollTop;
-    this.scrollingDown = currentScroll > this.lastScrollTop;
+    console.log('currentScroll', currentScroll);
+    this.scrollingDown = currentScroll > this.lastScrollTop; //Siempre compara el valor actual contra el anterior,En JavaScript (y TypeScript), las líneas de código se ejecutan en orden, una por una.
     this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
   }
 
