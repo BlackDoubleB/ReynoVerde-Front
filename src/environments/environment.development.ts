9d844a17ce087c535c988116c687cdbb21f9a4ec
BlackDoubleB <blancablacido@gmail.com>
2025-09-10 21:51:56 -0500

    test2

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,4 @@
 export const environment = {
-    apiURL: 'https://localhost:7071'
+    // apiURL: 'https://localhost:7071'
+    apiURL: 'https://webapireynoverde-b7g9hnc2d6f7brc8.canadacentral-01.azurewebsites.net' 
 };
